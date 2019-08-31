a50de508b6234901a62e9b67354e75e7b5ed8ded
Manny <mmunoz38@miners.utep.edu>
2019-08-30 18:17:08 -0600

    Sorted words, output to file

--- wordCounter.py
+++ wordCounter.py
@@ -1,14 +1,22 @@
 import re
 with open("declaration.txt", "r") as countFile:
+    #reads entire file as a string
+    #converts all characters to lowercase
+    #removes all punctuation from the string
+    #splits string into separate words
     data = re.sub(r'[^\w\s]', "", countFile.read().lower()).split()
 
 wordCounts = {}
 
 for word in data:
     if word not in wordCounts.keys():
+        #create a key for the word if does not exist
         wordCounts.update({word : 0})
     wordCounts[word] += 1
 
+#sort all the words alphabetically
+sortedWords = sorted(wordCounts.keys())
+
 with open("output.txt", "w") as outputFile:
-    for word in wordCounts.keys():
-        print(word + " " + str(wordCounts[word]))
+    for word in sortedWords:
+        outputFile.write(word + " " + str(wordCounts[word]) + "\n")
